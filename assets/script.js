23f46f59ef0a7f5613dd2ec5b5cca4a2aaee61a4
joey bennett <joeykb17@gmail.com>
2022-04-10 16:07:09 -0700

    add fixes to display and clear functions, remove excess variables

--- assets/script.js
+++ assets/script.js
@@ -13,33 +13,42 @@ var weatherResultsEl = $("weatherResults");
 
 if(localStorage.getItem("citiesArray") === null) {
     localStorage.setItem("citiesArray", JSON.stringify([]));
-    localStorage.setItem("searchedIndex", "0");
 } 
 
+// initialize the searched cities array from the local storage
 var searchedCities = JSON.parse(localStorage.getItem("citiesArray"));
-var searchedIndex = JSON.parse(localStorage.getItem("searchedIndex"));
+
+//now that we have our cities from the local storage, we can display them.
 displaySearchedCities();
 
-if($("#cityBtn")){
-    $("#cityBtn").on("click", showWeatherResults());
+//if there are no elements with the citybtn class don't listen for them
+//if there are, listen for a click event to show the weather results
+if($(".cityBtn")){
+    $(".cityBtn").on("click", showWeatherResults());
 }
 
+// this is an on click event listener for the search button
 searchBtnEl.on("click", function() {
+    //if the searchinput has any value
     if(searchInputEl.val()) {
+        //fetch the api geocoding for the city they typed in
         fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchInputEl.val() + '&limit=1&appid=8217350ca23bcfb8f4c4be20b0d654bd')
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // create an object with the name of the city, the latitude
+    // and the longitude
     var cityObj = {
         name: data[0].name,
         lat: data[0].lat,
         lon: data[0].lon
     }
-    console.log(searchedCities);
-    searchedCities.unshift(cityObj);
+    //clear the list of searched cities
+    clearSearchedCitiesList();
 
+    // add the newly added city to the front of the array
+    searchedCities.unshift(cityObj);
 
     //the reason we have the searched index is to keep the list of
     //cities from becoming too large, if we reach 10 cities
@@ -47,25 +56,23 @@ searchBtnEl.on("click", function() {
     //and continue to loop through the list replacing each
     //previous city
     searchedIndex++;
-    if(searchedIndex === 10) {
+    if(searchedCities.length > 9) {
         searchedCities.pop();
     }
-    localStorage.setItem("searchedIndex", searchedIndex);
     localStorage.setItem("citiesArray", JSON.stringify(searchedCities));
-    clearSearchedCitiesList();
     displaySearchedCities();
     showWeatherResults();
   });
     }
-})
+});
 
+//this function displays all the cities which
+//have been stored into local storage
 function displaySearchedCities() {
 
     for (var i = 0; i < searchedCities.length; i++) {
-        console.log("looping in the displaySearchedCities function");
         var tempCityBtnEl = $('<li>');
         tempCityBtnEl.text(searchedCities[i].name);
-        tempCityBtnEl.attr("index", searchedIndex);
         tempCityBtnEl.addClass("cityBtn");
         previousSearchResultsEl.append(tempCityBtnEl);
     
@@ -73,12 +80,12 @@ function displaySearchedCities() {
 
 }
 
+// this function checks if there are cities to clear, then
+//clears the list of cities
 function clearSearchedCitiesList() {
 
     if (previousSearchResultsEl.children.length) {
-        for (var i = searchedCities.length; i > 0; i--){
-            previousSearchResultsEl.children[i].remove();
-        }
+        previousSearchResultsEl.children().remove();
     }
 
 }
