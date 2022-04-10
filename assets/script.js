111e0d4ef727ec1202ad6b483831513e5a40fd5f
joey bennett <joeykb17@gmail.com>
2022-04-10 16:47:16 -0700

    add fetch for weather results function

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,8 @@ var weatherResultsEl = $("weatherResults");
 //a lat long to find the weather- we can use the 
 //geocoding api for this 
 
+var currentCity;
+
 if(localStorage.getItem("citiesArray") === null) {
     localStorage.setItem("citiesArray", JSON.stringify([]));
 } 
@@ -24,7 +26,12 @@ displaySearchedCities();
 //if there are no elements with the citybtn class don't listen for them
 //if there are, listen for a click event to show the weather results
 if($(".cityBtn")){
-    $(".cityBtn").on("click", showWeatherResults());
+    $(".cityBtn").on("click", function() {
+        var clicked = $(this);
+        console.log(clicked);
+        currentCity = searchedCities[clicked.attr("index")];
+        showWeatherResults(currentCity);
+    });
 }
 
 // this is an on click event listener for the search button
@@ -33,36 +40,37 @@ searchBtnEl.on("click", function() {
     if(searchInputEl.val()) {
         //fetch the api geocoding for the city they typed in
         fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + searchInputEl.val() + '&limit=1&appid=8217350ca23bcfb8f4c4be20b0d654bd')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    // create an object with the name of the city, the latitude
-    // and the longitude
-    var cityObj = {
-        name: data[0].name,
-        lat: data[0].lat,
-        lon: data[0].lon
-    }
-    //clear the list of searched cities
-    clearSearchedCitiesList();
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+        // create an object with the name of the city, the latitude
+        // and the longitude
+        var cityObj = {
+            name: data[0].name,
+            lat: data[0].lat,
+            lon: data[0].lon
+        }
+        currentCity = cityObj;
+        //clear the list of searched cities
+        clearSearchedCitiesList();
 
-    // add the newly added city to the front of the array
-    searchedCities.unshift(cityObj);
+        // add the newly added city to the front of the array
+        searchedCities.unshift(cityObj);
 
-    //the reason we have the searched index is to keep the list of
-    //cities from becoming too large, if we reach 10 cities
-    //we will go back to 0 and replace the first city
-    //and continue to loop through the list replacing each
-    //previous city
-    searchedIndex++;
-    if(searchedCities.length > 9) {
-        searchedCities.pop();
-    }
-    localStorage.setItem("citiesArray", JSON.stringify(searchedCities));
-    displaySearchedCities();
-    showWeatherResults();
-  });
+        //the reason we have the searched index is to keep the list of
+        //cities from becoming too large, if we reach 10 cities
+        //we will go back to 0 and replace the first city
+        //and continue to loop through the list replacing each
+        //previous city
+        searchedIndex++;
+        if(searchedCities.length > 9) {
+            searchedCities.pop();
+        }
+        localStorage.setItem("citiesArray", JSON.stringify(searchedCities));
+        displaySearchedCities();
+        showWeatherResults(currentCity);
+        });
     }
 });
 
@@ -74,6 +82,7 @@ function displaySearchedCities() {
         var tempCityBtnEl = $('<li>');
         tempCityBtnEl.text(searchedCities[i].name);
         tempCityBtnEl.addClass("cityBtn");
+        tempCityBtnEl.attr("index", i);
         previousSearchResultsEl.append(tempCityBtnEl);
     
     }
@@ -90,7 +99,17 @@ function clearSearchedCitiesList() {
 
 }
 
-function showWeatherResults() {
+function showWeatherResults(cityObj) {
+    weatherResultsEl.attr("style", "display:block");
+
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + cityObj.lat + '&lon=' + cityObj.lon + '&appid=8217350ca23bcfb8f4c4be20b0d654bd')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
 
 }
 //upon successful search we should do multiple tasks
