3877e802ca65d67f09d5dda4fdc771cfd679946a
joey bennett <joeykb17@gmail.com>
2022-04-10 01:02:26 -0700

    add localStorage variables, add functions to display/clear elements

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,18 @@ var weatherResultsEl = $("weatherResults");
 //a lat long to find the weather- we can use the 
 //geocoding api for this 
 
+if(localStorage.getItem("citiesArray") === null) {
+    localStorage.setItem("citiesArray", JSON.stringify([]));
+    localStorage.setItem("searchedIndex", "0");
+} 
 
+var searchedCities = JSON.parse(localStorage.getItem("citiesArray"));
+var searchedIndex = JSON.parse(localStorage.getItem("searchedIndex"));
+displaySearchedCities();
+
+if($("#cityBtn")){
+    $("#cityBtn").on("click", showWeatherResults());
+}
 
 searchBtnEl.on("click", function() {
     if(searchInputEl.val()) {
@@ -21,12 +32,57 @@ searchBtnEl.on("click", function() {
   })
   .then(function (data) {
     console.log(data);
-    var lat;
-    var long;
+    var cityObj = {
+        name: data[0].name,
+        lat: data[0].lat,
+        lon: data[0].lon
+    }
+    console.log(searchedCities);
+    searchedCities.unshift(cityObj);
+
+
+    //the reason we have the searched index is to keep the list of
+    //cities from becoming too large, if we reach 10 cities
+    //we will go back to 0 and replace the first city
+    //and continue to loop through the list replacing each
+    //previous city
+    searchedIndex++;
+    if(searchedIndex === 10) {
+        searchedCities.pop();
+    }
+    localStorage.setItem("searchedIndex", searchedIndex);
+    clearSearchedCitiesList();
+    displaySearchedCities();
   });
     }
 })
 
+function displaySearchedCities() {
+
+    for (var i = 0; i < searchedCities.length; i++) {
+        var tempCityBtnEl = $("<button>");
+        tempCityBtnEl.text(searchedCities[i].name);
+        tempCityBtnEl.attr("index", searchedIndex);
+        tempCityBtnEl.addClass("cityBtn");
+        previousSearchResultsEl.append(tempCityBtnEl);
+    
+    }
+
+}
+
+function clearSearchedCitiesList() {
+
+    if (previousSearchResultsEl.children.length) {
+        for (var i = searchedCities.length; i > 0; i--){
+            previousSearchResultsEl.children[i].remove();
+        }
+    }
+
+}
+
+function showWeatherResults() {
+    
+}
 //upon successful search we should do multiple tasks
     //create a button for the city we searched for
         //probably store this in local storage 
