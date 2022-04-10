f5beab11e5911d6defad6e53856a2b07093077ed
joey bennett <joeykb17@gmail.com>
2022-04-09 22:38:11 -0700

    add bootstrap add psuedocode plan

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//weather dashboard with search text input form
+//take input from user (city name) convert this to
+//a lat long to find the weather- we can use the 
+//geocoding api for this 
+
+//upon successful search we should do multiple tasks
+    //create a button for the city we searched for
+        //probably store this in local storage 
+    //display today's weather data for the city
+        //icon presenting today's weather
+        //temperature fahrenheit
+        //humidity
+        //wind speed
+        //uv index
+            //color presenting favorable, moderate or severe green yellow red
+    //5-day forecast
+        //date x/xx/xxxx
+        //weather icon
+        //temperature fahrenheit
+        //humidity
